0fa7cc3dc5100759fde2164270b25c4cc872149c
Eric Yarham <eric.yarham@gmail.com>
2023-07-20 10:16:24 -0400

    remove unused signinpanel reference

--- src/_common/Router.jsx
+++ src/_common/Router.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import SignInPanel from '../auth/SignInPanel';
+import PhoneSignIn from '../auth/phone/PhoneSignIn';
+import DatabaseDemo from '../pages/DatabaseDemo';
 import Home from './Home';
 import Layout from './Layout';
-import DatabaseDemo from '../pages/DatabaseDemo';
-import PhoneSignIn from '../auth/phone/PhoneSignIn';
 
 const Router = () => {
   return (
